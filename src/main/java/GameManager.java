0fffa320c60c21d1b3e5a7ddc0dbd177559ca71b
cesarion_pshebytski <yur6996@ukr.net>
2017-11-17 14:08:58 +0200

    updates

--- src/main/java/GameManager.java
+++ src/main/java/GameManager.java
@@ -3,19 +3,21 @@
 public class GameManager extends CharacterFactory {
     public void fight(Character c1, Character c2) {
         charactersStatus(c1, c2);
-        if (c1.getClass().equals(Hobbit.class)
-                && c1.getClass().equals(c2.getClass())) {
-            System.out.println("We cannot even kill ourselves :c");
-        } else {
-            while (c1.isAlive() && c2.isAlive()) {
-                c1.kick(c2);
-                charactersStatus(c1, c2);
-                Character temp = c2;
-                c2 = c1;
-                c1 = temp;
-            }
-            System.out.println(c1.isAlive() ? c1.getName() : c2.getName() + " wins!");
+        System.out.println(
+                (c1.equals(c2) && (c1 instanceof Hobbit || c2 instanceof Elf)?
+                        "We cannot even kill ourselves :c":
+                battle(c1,c2)));
+    }
+
+    private String battle(Character c1, Character c2) {
+        while (c1.isAlive() && c2.isAlive()) {
+            c1.kick(c2);
+            charactersStatus(c1, c2);
+            Character temp = c2;
+            c2 = c1;
+            c1 = temp;
         }
+        return c1.isAlive() ? c1.getName() : c2.getName() + " wins!";
     }
 
     private void charactersStatus(Character c1, Character c2) {
